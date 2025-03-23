70eefe03df3cc1c0ade2e1a5309e98c31b86b23e
goldenfox2025 <goldenfox2025@163.com>
2025-03-23 18:50:28 +0800

    微调

--- backend/cpp/src/CUDAoperators/multiply.cu
+++ backend/cpp/src/CUDAoperators/multiply.cu
@@ -1,16 +1,26 @@
-#include "cudaOP.cuh"
-#include <cstdio>
 #include <cuda_runtime.h>
-#include <iostream>
 #include <math.h>
+
+#include <cstdio>
+#include <iostream>
 #include <vector>
+
+#include "cudaOP.cuh"
 // --------------------------------------------------
 // multiply 算子实现（逐元素乘法）
 // --------------------------------------------------
 namespace cuda_OP {
 
-__global__ void multiply_kernel(const float *A, const float *B, float *out,
-                                int total) {
+__global__ void multiply_kernel_v2(const float *A, const float *B, float *out,
+                                   int total) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx < total) {
+    out[idx] = __ldg(&A[idx]) * __ldg(&B[idx]);
+  }
+}
+
+__global__ void multiply_kernel_v1(const float *A, const float *B, float *out,
+                                   int total) {
   int idx = blockIdx.x * blockDim.x + threadIdx.x;
   if (idx < total) {
     out[idx] = A[idx] * B[idx];
@@ -22,9 +32,9 @@ void multiply(Tensor<float> *output, const Tensor<float> *A,
   size_t total = A->numel();
   int threads = 256;
   int blocks = (total + threads - 1) / threads;
-  multiply_kernel<<<blocks, threads>>>(A->data_ptr(), B->data_ptr(),
-                                       output->data_ptr(), total);
+  multiply_kernel_v2<<<blocks, threads>>>(A->data_ptr(), B->data_ptr(),
+                                          output->data_ptr(), total);
   checkCudaError(cudaGetLastError());
   checkCudaError(cudaDeviceSynchronize());
 }
-} // namespace cuda_OP
+}  // namespace cuda_OP
