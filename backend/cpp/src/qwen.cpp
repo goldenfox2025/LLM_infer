7c2860962b724fc17442217f65623b0dee1af8e6
goldenfox2025 <goldenfox2025@163.com>
2025-04-08 18:25:31 +0800

    微调

--- backend/cpp/src/qwen.cpp
+++ backend/cpp/src/qwen.cpp
@@ -356,46 +356,28 @@ Tensor<T> QwenModel<T>::forward_cuda(const Tensor<uint32_t>* input,
       total_V = v_buf_view;
     }
 
-    // Tensor<T> att_heads_({n_heads_, head_dim_}, Device::CUDA);
     Tensor<T> att_heads({n_heads_, head_dim_}, Device::CUDA);
-    Tensor<T> att_scores({n_heads_, total_seq_len}, Device::CUDA);
 
     int ratio = n_heads_ / n_kv_heads_;
     for (int j = 0; j < 3; ++j) {
       cudaStreamSynchronize(compute_streams_[j]);
     }
 
-       cuda_OP::launch_gemmv(total_K.data_ptr(), Q_3d.data_ptr(),
+    // cuda_OP::flash_attention(Q_3d, total_K, total_V, att_heads);
+
+    Tensor<T> att_scores({n_heads_, total_seq_len}, Device::CUDA);
+    // cuda_OP::compute_attention_scores(Q_3d, total_K, n_heads_, head_dim_,
+    //                                   att_scores, n_kv_heads_);
+
+    cuda_OP::launch_gemmv(total_K.data_ptr(), Q_3d.data_ptr(),
                           att_scores.data_ptr(), n_heads_, ratio, total_seq_len,
                           head_dim_, head_dim_, total_seq_len);
 
     cuda_OP::softmax(&att_scores, &att_scores, /*dim=*/1, false, offset);
 
-    // cuda_OP::flash_attention(Q_3d, total_K, total_V, att_heads_);
-
-    // Tensor<T> att_scores({n_heads_, total_seq_len}, Device::CUDA);
-
-    // cuda_OP::compute_attention_scores(Q_3d, total_K, n_heads_, head_dim_,
-    //                                   att_scores, n_kv_heads_);
-    // // debugPrintTensor(att_scores,
-    // //                  "attention scores (layer " + std::to_string(i) +
-    // // ")");
-
-    // // Softmax处理注意力分数
-    // cuda_OP::softmax(&att_scores, &att_scores, /*dim=*/1, false, offset);
-
-    // // 计算注意力输出
-    // Tensor<T> att_heads({n_heads_, head_dim_}, Device::CUDA);
-
     cuda_OP::compute_att_output(att_scores, total_V, n_heads_, head_dim_,
                                 att_heads, n_kv_heads_);
-    // debugPrintTensor(att_heads,
-    //                  "attention heads (layer " + std::to_string(i) + ")");
 
-    // debugPrintTensor(att_heads_,
-    //                  "flash_attention heads (layer " + std::to_string(i) +
-    //                  ")");
-    // 投影回原始维度
     Tensor<T> att_heads_reshaped = att_heads.view({1, n_heads_ * head_dim_});
     Tensor<T> att_proj({1, hidden_size_}, Device::CUDA);
     cuda_OP::matmul(att_heads_reshaped, wo, &att_proj, nullptr, o_bias);
